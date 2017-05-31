f7303e7d4a2226c74c175f8585209e856eb617ac
alex123bob <alex123bob@126.com>
2017-05-31 17:59:43 +0800

    add home page.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Sidebar from 'containers/sidebar/sidebar';
 import ContentCt from 'containers/contentct/index';
+import Home from 'components/home';
 
 // Locale related
 import { addLocaleData, IntlProvider } from 'react-intl';
@@ -15,20 +16,33 @@ addLocaleData(localeData);
 
 const { locale, messages } = __I18N__;
 
-const App = ({ match }) => (
-    <IntlProvider
-        locale={ locale }
-        messages={ messages }
-    >
-        <div className='main-app'>
-            <Sidebar />
-            <Route path='/:module' component={ContentCt} />
-        </div>
-    </IntlProvider>
-);
+class App extends Component {
+    state = {
+        collapsed: false,
+    };
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+    render() {
+        return (
+            <IntlProvider
+                locale={ locale }
+                messages={ messages }
+            >
+                <div className='main-app'>
+                    <Sidebar />
+                    <Route exact path='/' component={Home} />
+                    <Route path='/:module' component={ContentCt} />
+                </div>
+            </IntlProvider>
+        );
+    }
+}
 
 App.propTypes = {
-    match: PropTypes.object.isRequired
+    match: PropTypes.object.isRequired,
 };
 
 export default App;
